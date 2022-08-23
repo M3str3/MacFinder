fead56494c693e7f52382e65a132bbf5e81fc90e
Nacho Mestre <namestre3@gmail.com>
2022-08-23 21:05:58 +0200

    Updating functions & Translating some variables to ENG

--- MacFinder.py
+++ MacFinder.py
@@ -1,37 +1,44 @@
 #Python 3.7.9
-try:
-    from art import *
-    from termcolor import colored
-    tprint('MacFinder', font="sub-zero")
-    print(colored('Created by Mestre',"green"))
-except:
-    print('')
-
-from email import header
+from datetime import datetime
 import os
 import argparse
+import sys
 from time import sleep
 
-macs=[]
+###########################################################################
 
+macs=[]
+vendors={}
+vendor_macs=[]
+
+###########################################################################
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", help="fichero")
+parser.add_argument("-f", "--file", help="inputFile")
 parser.add_argument("-o", "--output", help="output")
 args = parser.parse_args()
 argv = vars(args)
-args_fichero = argv['file']
+args_inputFile = argv['file']
 args_output = argv['output']
 
-if args.file:
-    fichero=args_fichero
-else:
-    print('[*]ERROR - Pls define a file with "-f", u can define your output with "-o" ')
-    print('EXAMPLE: python MacFinder.py -f maclist.txt -o output.txt')
-    quit()
+def requestVendor(mc):
+    Command = 'curl https://api.macvendors.com/'+mac
+    response=os.popen(Command).read()
+    return response
 
-fichero = open (fichero, 'r')
-texto = fichero.read()
-macs=texto.split("\n")
+if args.file:
+    inputFile=args_inputFile
+    inputFile = open (inputFile, 'r')
+    texto = inputFile.read()
+    macs=texto.split("\n")
+else:
+    print('\t[INFO] You can define a input file with "-f", and, you can define your output with "-o" ')
+    print('\tEXAMPLE: python MacFinder.py -f maclist.txt -o output.txt \n\n')
+    main=True
+    while main:
+        mac = input(f'{datetime.now()} | Write mac address or "exit" to close \n{datetime.now()} | ')
+        if mac.lower() == 'exit':
+            sys.exit()
+        print(f'{datetime.now()} | {mac} => {requestVendor(mac)}\n')
 
 if args.output:
     output_file=open(args_output,'w')
@@ -39,11 +46,24 @@ else:
     output_file=None
 
 for mac in macs:
-    comando = 'curl https://api.macvendors.com/'+mac
-    resp=os.popen(comando).read()
-    if output_file not None:
-        output_file.write(mac+" ; "+(resp)+"\n")
-    print(f"\n Mac -> {mac} Fabricante -> {resp} \n")
+    resp = requestVendor(mac)
     sleep(1.1)
+    if len(resp)>60:
+        continue
+    if '{"errors":{"detail":"Not Found"}}' in resp:
+        resp='Unknown'
+    if output_file is not None:
+        output_file.write(f'[{mac}] - {resp} \n')
+    if resp not in vendors:
+        vendors[resp]=[]
+    vendors[resp].append(mac)
+    print(f"\n Mac -> {mac} Vendor -> {resp} \n")
+    
+print(f'{datetime.now()} | RESUME')
+print(f'-----------------------------------')
+for vend in vendors:
+    print(f'{datetime.now()} | Vendor {vend}')
+    for mc in vendors[vend]:
+        print(f'{datetime.now()} | \t╚{mc}')
 
     
